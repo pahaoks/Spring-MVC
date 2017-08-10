e53424f25dc2adc132b339031a54e63d49f44aea
Pasha <pahaoks@gmail.com>
2017-08-11 00:00:01 +0300

    Fix Security by adding SpringSecurityInit.java

--- src/main/java/ru/javastudy/mvcHtml5Angular/javaconfig/SecurityConfig.java
+++ src/main/java/ru/javastudy/mvcHtml5Angular/javaconfig/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 import javax.sql.DataSource;
 
@@ -26,13 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("SELECT USERNAME, PASSWORD, ENABLED FROM USER WHERE USERNAME=?")
                 .authoritiesByUsernameQuery("SELECT U.USERNAME, A.AUTHORITY\n" +
-                        "        \t FROM AUTHORITIES A, USER U WHERE U.USERNAME = A.USERNAME AND U.USERNAME = ?");;
+                        "        \t FROM AUTHORITIES A, USER U WHERE U.USERNAME = A.USERNAME AND U.USERNAME = ?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-//                .anyRequest().authenticated() //all requests will checked
+                //.anyRequest().authenticated() //all requests will checked
                 .and()
                 .formLogin().loginPage("/login.html").permitAll().usernameParameter("j_username")
                 .passwordParameter("j_password").loginProcessingUrl("/j_spring_security_check").failureUrl("/login.html?error=true")
@@ -47,6 +48,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe().key("myKey").tokenValiditySeconds(300)
                 .and()
                 .csrf().disable();
-
     }
 }
